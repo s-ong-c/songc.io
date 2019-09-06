c455d1140cdb8f86735be64c65bc1ab4f02de00a
s-ong-c <somony9292@gmail.com>
2019-09-07 01:14:34 +0900

    삽질 중..

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,27 +3,27 @@ module.exports = {
   description: `Blog posted about ...`,
   author: `songc`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://s-ong-c.github.io`, // Your blog site url
+  siteUrl: `https://s-ong-c/s-ong-c.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `s-ong-c`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: `` // Your Facebook account
+    facebook: ``, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: "", // Your disqus-short-name. check disqus.com.
-    utterances: "s-ong-c/s-ong-c.github.io" // Your repository for archive comment
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 's-ong-c/s-ong-c.github.io', // Your repository for archive comment
   },
   configs: {
-    countOfInitialPost: 10 // Config your initial count of post
+    countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: "jbee"
+    buyMeACoffeeId: 'jbee',
   },
   share: {
-    facebookAppId: "" // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: "" // Add your google analytics tranking ID
-};
+  ga: '', // Add your google analytics tranking ID
+}
