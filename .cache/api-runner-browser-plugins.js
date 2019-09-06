abb65f9c393e05f1b341b330e6396b524706dc8d
s-ong-c <somony9292@gmail.com>
2019-09-07 01:04:54 +0900

    수정

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,22 +1,22 @@
 module.exports = [{
-      plugin: require('/blog/s-ong-c.github.io/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      plugin: require('/Users/songminseok/s-ong-c.github.io/node_modules/gatsby-remark-images/gatsby-browser.js'),
       options: {"plugins":[],"maxWidth":590,"linkImagesToOriginal":false},
     },{
-      plugin: require('/blog/s-ong-c.github.io/node_modules/gatsby-remark-images-medium-zoom/gatsby-browser.js'),
+      plugin: require('/Users/songminseok/s-ong-c.github.io/node_modules/gatsby-remark-images-medium-zoom/gatsby-browser.js'),
       options: {"plugins":[],"margin":36,"scrollOffset":0},
     },{
-      plugin: require('/blog/s-ong-c.github.io/node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
+      plugin: require('/Users/songminseok/s-ong-c.github.io/node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/blog/s-ong-c.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      plugin: require('/Users/songminseok/s-ong-c.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
       options: {"plugins":[],"trackingId":""},
     },{
-      plugin: require('/blog/s-ong-c.github.io/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      plugin: require('/Users/songminseok/s-ong-c.github.io/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
       options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
     },{
-      plugin: require('/blog/s-ong-c.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      plugin: require('/Users/songminseok/s-ong-c.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/blog/s-ong-c.github.io/gatsby-browser.js'),
+      plugin: require('/Users/songminseok/s-ong-c.github.io/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
