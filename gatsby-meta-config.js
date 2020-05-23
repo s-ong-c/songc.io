626611d13add1abd85d3b22a2a69af6bb6342114
s-ong-c <somony9292@gmail.com>
2020-05-23 16:49:51 +0900

    buyMeACoffeeId 연결

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -21,7 +21,7 @@ module.exports = {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: '',
+    buyMeACoffeeId: 'songc',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
