0ebe3635d081c28d510c2b78201ece02cc2e5d83
s-ong-c <somony9292@gmail.com>
2020-05-23 16:25:28 +0900

    ES2020 Post upload

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -21,7 +21,7 @@ module.exports = {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
