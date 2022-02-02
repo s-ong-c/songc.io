cf8ef397cef5d25f6097240b8651afbf7b69639e
s-ong-c <somony9292@gmail.com>
2022-02-02 18:38:16 +0900

    feat: 2021.log post

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,30 +1,30 @@
 var plugins = [{
       name: 'gatsby-remark-autolink-headers',
-      plugin: require('/Users/songminseok/blog/s-ong-c.github.io/node_modules/gatsby-remark-autolink-headers/gatsby-ssr'),
+      plugin: require('/Users/songc/playground/songc/songc.io/node_modules/gatsby-remark-autolink-headers/gatsby-ssr'),
       options: {"plugins":[],"offsetY":0,"className":"anchor"},
     },{
       name: 'gatsby-plugin-google-analytics',
-      plugin: require('/Users/songminseok/blog/s-ong-c.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
+      plugin: require('/Users/songc/playground/songc/songc.io/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
       options: {"plugins":[],"trackingId":"UA-134332040-1","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0,"enableWebVitalsTracking":false},
     },{
       name: 'gatsby-plugin-manifest',
-      plugin: require('/Users/songminseok/blog/s-ong-c.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/songc/playground/songc/songc.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"SONGC","short_name":"SONGC","start_url":"/","background_color":"#ffffff","theme_color":"#663399","display":"minimal-ui","icon":"content/assets/felog.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"2060af118e2d3921b1af8888668409c1"},
     },{
       name: 'gatsby-plugin-typography',
-      plugin: require('/Users/songminseok/blog/s-ong-c.github.io/node_modules/gatsby-plugin-typography/gatsby-ssr'),
+      plugin: require('/Users/songc/playground/songc/songc.io/node_modules/gatsby-plugin-typography/gatsby-ssr'),
       options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
     },{
       name: 'gatsby-plugin-offline',
-      plugin: require('/Users/songminseok/blog/s-ong-c.github.io/node_modules/gatsby-plugin-offline/gatsby-ssr'),
+      plugin: require('/Users/songc/playground/songc/songc.io/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-react-helmet',
-      plugin: require('/Users/songminseok/blog/s-ong-c.github.io/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/songc/playground/songc/songc.io/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-sitemap',
-      plugin: require('/Users/songminseok/blog/s-ong-c.github.io/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
+      plugin: require('/Users/songc/playground/songc/songc.io/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
       options: {"plugins":[],"output":"/sitemap","createLinkInHead":true,"entryLimit":45000,"query":"{ site { siteMetadata { siteUrl } } allSitePage { nodes { path } } }","excludes":[]},
     }]
 /* global plugins */
